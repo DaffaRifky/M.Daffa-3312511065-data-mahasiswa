36b9d47981958a72173bcf5a6498905690fb0842
DaffaRifky <dafarifky9@gmail.com>
2026-09-21 20:38:06 +0700

    alright, finally found the fix, i hope

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<REPO>/",
+  base: '/M.Daffa-3312511065-data-mahasiswa/',
 })
